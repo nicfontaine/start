ec5926f16b83e6ece17370e54e18c9a3270c3b77
Nic Fontaine <ngpfontaine@gmail.com>
2016-11-18 17:12:32 -0800

    touch swipe for search swap

--- main.js
+++ main.js
@@ -632,7 +632,6 @@ searchInputDom.addEventListener('keydown', function(e) {
 	if (e.keyCode == '37') {
 		e.preventDefault();
 		searchSwitch('l');
-
 	}
 
 	// KEY RIGHT
@@ -923,3 +922,47 @@ function weatherSwap(e,t) {
 	navCityDom.innerHTML = weatherCity;
 
 }
+
+// TOUCH SWIPES
+searchInputDom.addEventListener('touchstart', handleTouchStart, false);        
+searchInputDom.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;                                                        
+
+function handleTouchStart(evt) {                                         
+  xDown = evt.touches[0].clientX;                                      
+  yDown = evt.touches[0].clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+  if ( ! xDown || ! yDown ) {
+      return;
+  }
+
+  var xUp = evt.touches[0].clientX;                                    
+  var yUp = evt.touches[0].clientY;
+
+  var xDiff = xDown - xUp;
+  var yDiff = yDown - yUp;
+
+  if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+    if ( xDiff > 0 ) {
+    	/* left swipe */ 
+    	searchSwitch('r');
+    } else {
+    	/* right swipe */
+    	searchSwitch('l');
+    }                       
+  }
+  else {
+    if ( yDiff > 0 ) {
+        /* up swipe */ 
+    } else { 
+        /* down swipe */
+    }                                                                 
+  }
+  /* reset values */
+  xDown = null;
+  yDown = null;                                             
+};
