ba09a051ca537f7be271233642f4cf2e13210271
Nic Fontaine <ngpfontaine@gmail.com>
2016-11-21 21:17:17 -0800

    tile links, & re-added swipe code...? wtf git

--- main.js
+++ main.js
@@ -895,3 +895,48 @@ function weatherSwap(e,t) {
 	navCityDom.innerHTML = weatherCity;
 
 }
+
+// TOUCH SWIPES
+si.inputD.addEventListener('touchstart', handleTouchStart, false);        
+si.inputD.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;                                                        
+
+function handleTouchStart(evt) {                                         
+  xDown = evt.touches[0].clientX;                                      
+  yDown = evt.touches[0].clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+  if ( ! xDown || ! yDown ) {
+		return;
+  }
+
+  var xUp = evt.touches[0].clientX;                                    
+  var yUp = evt.touches[0].clientY;
+
+  var xDiff = xDown - xUp;
+  var yDiff = yDown - yUp;
+
+  if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+    if ( xDiff > 0 ) {
+    	console.log(xDiff);
+    	/* left swipe */ 
+    	searchSwitch('r');
+    } else {
+    	/* right swipe */
+    	searchSwitch('l');
+    }                       
+  }
+  else {
+    if ( yDiff > 0 ) {
+        /* up swipe */ 
+    } else { 
+        /* down swipe */
+    }                                                                 
+  }
+  /* reset values */
+  xDown = null;
+  yDown = null;                                             
+};
