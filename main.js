3d731ee13102a97e8954ac05110f8580c08d876f
nic fontaine <ngpfontaine@gmial.com>
2016-12-23 13:34:42 -0800

    placement for random hue & var

--- main.js
+++ main.js
@@ -6,14 +6,13 @@
 // BASE COLOUR SETTINGS
 //
 
-function randomIntFromInterval(min,max)
-{
-    return Math.floor(Math.random()*(max-min+1)+min);
+function randomIntFromInterval(min,max) {
+  return Math.floor(Math.random()*(max-min+1)+min);
 }
 
 // HUE, 0-360
-// var cpbh = 200;
 var cpbh = randomIntFromInterval(0,360);
+// var cpbh = 200;
 
 // (NOTE TO-DO) LIGHT/DARK MODE
 // var cMode = 'dark';
